da90d696bbdec692619febc39034ac8e83e472af
denisYordanov-eng <denisivov437@gmail.com>
2025-05-23 10:41:49 +0300

    Made a task.

---
+++ 05-WriteToFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace _05_WriteToFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string allLines = File.ReadAllText
+                (@"C:\Users\Laptop\Desktop\C#\ObjectsClassesFilesAndExceptions-More Exercises\sample_text.txt");
+            
+            char[] chars = new char[] { '.', ',', '!','?',':' };
+
+            string withoutPunctuation = new string(allLines.Where(c => !chars.Contains(c)).ToArray());
+
+
+            File.WriteAllText("output.txt",withoutPunctuation);
+        }
+    }
+}
