8ff9edd289280199a5667289f3f7356fe5fa5aba
denisYordanov-eng <denisivov437@gmail.com>
2025-05-23 10:21:09 +0300

    Made a task.

---
+++ 04-PunctuationFinder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_PunctuationFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          string[] allLines =  File.ReadAllLines(@"C:\Users\Laptop\Desktop\C#\ObjectsClassesFilesAndExceptions-More Exercises\sample_text.txt");
+            List<char> punctuatios = new List<char>();
+            char[] punctuatiosToTake = new[] { '.', ',', '!', '?', ':' };    
+
+            foreach(var line in allLines)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (punctuatiosToTake.Contains(line[i]))
+                    {
+                        punctuatios.Add(line[i]);
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(", ",punctuatios));
+        }
+    }
+}
