1cbf58f01a3b3947ab9654cdc8b2fa5018a87e00
denisYordanov-eng <denisivov437@gmail.com>
2025-05-24 11:09:51 +0300

    Made a task.

---
+++ 07-PlayCatch/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_PlayCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = Console.ReadLine()
+                .Split(' ')
+                .Select(n => int.Parse(n))
+                .ToArray();
+            int exetionCounter = 0;
+
+            while (true)
+            {
+                if (exetionCounter == 3)
+                {
+                    break;
+                }
+                string[] token = Console.ReadLine().Split(' ');
+
+                string command = token[0];
+               
+
+                    if (command == "Replace")
+                    {
+                    try
+                    {
+                        int index = int.Parse(token[1]);
+                        int element = int.Parse(token[2]);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("The variable is not in the correct format!");
+                        exetionCounter++;
+                        continue;
+                    }
+                        try
+                        {
+                            int index = int.Parse(token[1]);
+                            int element = int.Parse(token[2]);
+                            number[index] = element;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("The index does not exist!");
+                            exetionCounter++;                          
+                        }
+                    }
+
+                else if (command == "Print")
+                {
+                    try
+                    {
+                        int startIndex = int.Parse(token[1]);
+                        int endIndex = int.Parse(token[2]);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("The variable is not in the correct format!");
+                        exetionCounter++;
+                        continue;
+                    }
+                    try
+                    {
+                        int startIndex = int.Parse(token[1]);
+                        int endIndex = int.Parse(token[2]);
+                        List<int> numbers = new List<int>();
+                        for (int i = startIndex; i < endIndex; i++)
+                        {
+                            numbers.Add(number[i]);
+                        }
+                        Console.WriteLine(String.Join(", ", numbers));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("The index does not exist!");
+                        exetionCounter++;
+                    }
+                }
+                
+                else if (command == "Show")
+                    {
+                    try
+                    {
+                        int index = int.Parse(token[1]);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("The variable is not in the correct format!");
+                        exetionCounter++; 
+                        continue;
+                    }
+                        try
+                        {
+                            int index = int.Parse(token[1]);
+                            Console.WriteLine(number[index]);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("The index does not exist!");
+                            exetionCounter++;
+                        }
+                    }
+            }
+            Console.WriteLine(String.Join(", ", number));
+        }
+    }
+}
