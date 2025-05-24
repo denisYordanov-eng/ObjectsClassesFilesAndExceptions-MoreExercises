cee4bbe360efee4f6e9f53198fea3f3a45588dd6
denisYordanov-eng <denisivov437@gmail.com>
2025-05-24 09:34:14 +0300

    Made a task.

---
+++ 08-PersonalExeption/PersonalExeption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_PersonalExeption
+{
+    internal class PersonalExeption : Exception
+    {
+        public PersonalExeption() : base("My first exception is awesome!!!")
+        {
+
+        }
+    }
+}
---
+++ 08-PersonalExeption/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_PersonalExeption
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                int number = int.Parse(Console.ReadLine());
+                try
+                {
+                    if (number < 0)
+                    {
+                        throw new PersonalExeption();
+                    }
+                }
+                catch (PersonalExeption pe)
+                {
+                    Console.WriteLine(pe.Message);
+                    return;
+                }
+            }
+        }
+    }
+}
