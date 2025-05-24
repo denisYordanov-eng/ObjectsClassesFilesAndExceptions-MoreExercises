026e3dbc87d6b0737860fcec7144b2304c61ebb9
denisYordanov-eng <denisivov437@gmail.com>
2025-05-24 11:10:37 +0300

    Made a task using different catch methods.

---
+++ 07-PlayCatch-betterVersion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_PlayCatch_betterVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(n => int.Parse(n))
+                .ToArray();
+
+            int exeptionCounter = 0;
+            while (exeptionCounter < 3)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                string command = tokens[0];
+                if()
+            }
+            
+        }
+    }
+}
